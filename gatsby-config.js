054061bb7b5d8b5c8dc5956ea603808972ea31aa
an0i <1196557571@qq.com>
2023-05-22 09:28:04 +0800

    feat: install sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'an0i\'s blog',
     description: 'an0i\'s blog',
     icon: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-2 -2 54 54'%3e%3cpath d='M 0 25 A 1 1 0 0 0 50 25 A 1 1 0 0 0 0 25 Z' stroke='%231668d6' stroke-width='2.5' fill='none'/%3e%3cpath d='M -1 34 Q 3 27 10 30 Q 10 25 20 17 Q 19 23 17 25 Q 30 9 44 4 Q 36 12 29 18 Q 36 13 37 15 Q 34 19 23 25 Q 30 22 33 24 Q 27 27 18 31 Q 24 30 24 32 Q 22 33 18 34 Q 19 34 21 36 L 42 36 L 49 36 L 49 37 L 42 37 L 23 37 Q 12 41 4 33 Z' fill='%232877e0'/%3e%3c/svg%3e",
+    siteUrl: 'https://blog.anqi.eu.org',
   },
   plugins: [
     {
@@ -33,5 +34,6 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-transformer-sharp',
     'gatsby-plugin-postcss',
+    'gatsby-plugin-sitemap',
   ],
 };
